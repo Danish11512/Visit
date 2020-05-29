b862cee2e0d1b14f98364bba4569aae34a116d52
Danish Faruqi <dfaruqi@records.nyc.gov>
2020-05-29 13:29:35 -0400

    main page skeleton done

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
-
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 
 
 from .main import views
+
+bootstrap = Bootstrap(app)
     
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,22 @@
 from app import app
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
-from flask_wtf import Form
-
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+# @app.route('/login')
+# def login():
+#     return render_template('login.html')
+
+# @app.route('/signup')
+# def signup():
+#     return render_template('signup.html')
+
+# @app.route('/dashboard')
+# def dashboard():
+#     return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

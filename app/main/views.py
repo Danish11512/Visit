8d9088102d169ab81cafaf8aba6561e3e007d004
Danish Faruqi <dfaruqi@records.nyc.gov>
2020-06-04 13:26:33 -0400

    HTML fixes

--- app/main/views.py
+++ app/main/views.py
@@ -17,7 +17,7 @@ def index():
 @app.route('/login', methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
+    if form.validate_on_submit():               
         return redirect(url_for('login'))
 
     return render_template('login.html', form=form)

864db9507a2062bae033984810233353a6ebf498
munifnagi <mnagi@records.nyc.gov>
2020-06-19 11:10:10 -0400

    still need to fix mail server config

--- config.py
+++ config.py
@@ -9,6 +9,12 @@ class Config():
     )
     EMAIL_DOMAIN = 'records.nyc.gov'
     ADMIN = os.environ.get("ADMIN") or "admin@records.nyc.gov"
-
+    MAIL_SERVER = os.environ.get("MAIL_SERVER") or "localhost"
+    MAIL_PORT = os.environ.get("MAIL_PORT") or 1111
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") or False
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME") or None
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") or None
+    MAIL_SUBJECT_PREFIX = "[Visit]"
+    MAIL_SENDER = "Records Visit <admin@records.nyc.gov>"
 
     

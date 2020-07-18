bac71994e97ccd9838da3984e247e3051ae074e7
Danish Faruqi <danish.faruqi1@gmail.com>
2020-07-18 01:01:42 -0400

    removed secret key

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config():
-    SECRET_KEY = (os.environ.get("SECRET_KEY") or "cityofnewyorkkeythatissecretandhardtoguess")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get("DATABASE_URL")

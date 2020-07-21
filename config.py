3aff51603ab90fcf18b7bece72a87c2eded8b91b
Danish Faruqi <dfaruqi@records.nyc.gov>
2020-07-21 11:43:01 -0400

    added key

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config():
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY") or 'verysecretkey'
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get("DATABASE_URL")
